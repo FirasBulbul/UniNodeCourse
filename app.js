3d410611556ee97b4d579e47521b82694786cfdb
Firas <106191320+FirasBulbul@users.noreply.github.com>
2023-04-27 23:51:18 +0300

    ...

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const http = require ('http'); // importing http library to creat a server to run JS in server-side
-const wc = require ('./WordCounter')
+const http = require('http'); // importing http library to creat a server to run JS in server-side
+// const wc = require('./WordCounter')
 
-// const port = 4000; //port to run the server but shouldn't being between 0 and 1024
-// const server = http.createServer();
+// console.log(wc('Firas Abdulrahman Bulbul'));
 
-// server.listen(port, () => { // running the server to get the responses
-//     console.log('Server is litening right now....');
-// })
+const port = 4000; //port to run the server but shouldn't being between 0 and 1024
+const server = http.createServer();
 
-console.log(wc('Firas Abdulrahman Bulbul'));
+server.listen(port, () => { // running the server to get the responses
+    console.log('Server is litening right now....');
+})
 
