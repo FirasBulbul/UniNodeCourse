d053f78ffea68da6c0e44747052b686976baf371
Firas <106191320+FirasBulbul@users.noreply.github.com>
2023-05-02 23:05:01 +0300

    new change

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-// const http = require('http'); // importing http library to creat a server to run JS in server-side
-// const wc = require('./WordCounter')
-
-// console.log(wc('Firas Abdulrahman Bulbul'));
-
-// const port = 4000; //port to run the server but shouldn't being between 0 and 1024
-// const server = http.createServer();
-
-// server.listen(port, () => { // running the server to get the responses
-//     console.log('Server is litening right now....');
-// })
+const http = require('http');
 
