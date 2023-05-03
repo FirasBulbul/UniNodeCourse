75e80ee3372f49a9e4d4b67cf0e79afe61da33ab
Firas <106191320+FirasBulbul@users.noreply.github.com>
2023-05-03 10:20:35 +0300

    new

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Hello3')
+}
+
+app.get('/chain', [one, two, three]);
+
+app.listen(PORT, () => {
+    console.log(`Server is listen throw ${PORT} port`);
+})
